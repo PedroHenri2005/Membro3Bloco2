eae54fd8d3ff0f974aa8b647b7342152ac3cf7f2
pedrohenri2005 <cologh@usp.br>
2026-05-31 13:06:24 -0300

    main.py agora aceitando legendas automáticas e com rotas para criação e manutenção de Decks e Cards

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Form, HTTPException 
+from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
