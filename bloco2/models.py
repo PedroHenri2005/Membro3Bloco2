d770f41406a616b14ffbe90b93f5673b29da7afb
pedrohenri2005 <cologh@usp.br>
2026-05-31 12:50:05 -0300

    Novo models.py para definir os Decks e Cards

--- bloco2/models.py
+++ bloco2/models.py
@@ -1,25 +1,25 @@
-# [ MEMBRO 3 - BLOCO 3]
-# Aqui é onde ficarão as classes que serão usadas para salvar os Flashcards de revisão, e organizá-los em Decks.
+# [ MEMBRO 3 - BLOCO 3 ]: Aqui é onde ficarão as classes que serão usadas para salvar os Cards de revisão, que serão organizados em Decks:
 from datetime import datetime, timezone
 from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 
 class DeckVideo(SQLModel, table=True):
     """Agrupa os cards gerados por um vídeo do YouTube em um único Deck."""
-    video_id: str = Field(primary_key=True, index=True)
-    titulo: Optional[str] = None
-    
-    cards: List["Card"] = Relationship(back_populates="deck_video")
+    video_id: str = Field(primary_key=True, index=True) # O ID do vídeo é a própria ID única de 11 dígitos que caracteriza ele na URL do Youtube.
+    titulo: Optional[str] = None # O título do vídeo do Youtube.
+    cards: List["Card"] = Relationship(back_populates="deck_video", cascade_delete = True) # Aqui, relaciono o Deck com todos os Cards dele.
 
 class Card(SQLModel, table=True):
-    """Modelo dos flashcards salvos para revisão espaçada."""
+    """Modelo dos Cards salvos para revisão espaçada."""
     id: Optional[int] = Field(default=None, primary_key=True)
     texto_legenda: str = Field(min_length=1)
     start_time: float
     end_time: float
+    # Por padrão, a data de criação e de revisão de um Card novo será o momento atual:
     data_criacao: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     data_proxima_revisao: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    # Adicionei também algumas outras propriedades que podem ser úteis no futuro:
+
+    # Adicionei também algumas outras propriedades que podem ser úteis no futuro, para implementar um algoritmo de repetição espaçada:
     
     # Histórico de acertos seguidos (essencial para resetar ou avançar o Card):
     revisoes_consecutivas: int = Field(default=0)
